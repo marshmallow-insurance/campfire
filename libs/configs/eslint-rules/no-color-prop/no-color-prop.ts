2ef842126be09dd6dc2b58a117944faab966da96
Liam Canetti <git@liamcanetti.co.uk>
2025-10-03 13:58:57 +0100

    feat(design-system): custom eslint rules

---
+++ libs/configs/eslint-rules/no-color-prop/no-color-prop.ts
@@ -0,0 +1,79 @@
+import { TSESTree, ESLintUtils } from '@typescript-eslint/utils'
+
+const COLOR_KEYS = [
+  'lollipop',
+  'marshmallowPink',
+  'bubblegum',
+  'fairyFloss',
+  'boba',
+  'liquorice',
+  'sesame',
+  'chia',
+  'custard',
+  'mascarpone',
+  'coconut',
+  'cream',
+  'spearmint',
+  'feijoa',
+  'blueberry',
+  'macaroon',
+  'pistachio',
+  'matcha',
+  'caramel',
+  'peanut',
+  'marzipan',
+  'oatmeal',
+  'satsuma',
+  'strawberry',
+  'watermelon',
+  'apple',
+  'mint',
+  'lemon',
+  'sherbert',
+  'tangerine',
+  'compareTheMarket',
+  'confused',
+  'onfido',
+  'x',
+  'premfina',
+  'checkout',
+  'meta',
+  'stripe',
+  'intercom',
+  'ravelin',
+  'rac',
+  'hometree',
+]
+
+export const noColorPropRule = ESLintUtils.RuleCreator((name) => `${name}`)({
+  name: 'no-color-prop',
+  meta: {
+    type: 'problem',
+    docs: {
+      description:
+        'Disallow Color props (any prop) on design system components',
+    },
+    messages: {
+      noColorProp:
+        'Passing a design-system `Color` ("{{val}}") as a prop is deprecated. Use styled-components theme tokens instead.',
+    },
+    schema: [],
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      JSXAttribute(node: TSESTree.JSXAttribute) {
+        if (node.value?.type === 'Literal') {
+          const val = String(node.value.value)
+          if (COLOR_KEYS.includes(val)) {
+            context.report({
+              node,
+              messageId: 'noColorProp',
+              data: { val },
+            })
+          }
+        }
+      },
+    }
+  },
+})
---
+++ libs/configs/eslint-rules/no-theme-colors/no-theme-colors.ts
@@ -0,0 +1,31 @@
+import { TSESTree, ESLintUtils } from '@typescript-eslint/utils'
+
+export const noThemeColorsRule = ESLintUtils.RuleCreator((name) => `${name}`)({
+  name: 'no-theme-colors',
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'Disallow usage of theme.colors',
+    },
+    messages: {
+      noThemeColors:
+        'Usage of `theme.colors` is deprecated. Use the new styled-components theme instead.',
+    },
+    schema: [],
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      MemberExpression(node: TSESTree.MemberExpression) {
+        if (
+          node.object.type === 'Identifier' &&
+          node.object.name === 'theme' &&
+          node.property.type === 'Identifier' &&
+          node.property.name === 'colors'
+        ) {
+          context.report({ node, messageId: 'noThemeColors' })
+        }
+      },
+    }
+  },
+})
